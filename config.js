dadf6d2b780cec9599a82d4ba9cd524468b80d02
Karthik Gowda M S <karthikgowdams27@gmail.com>
2024-10-26 22:52:28 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // const CLIENT_URL="http://localhost:3000"
-const CLIENT_URL="https://drugify.karthikgowdams.com"
+const CLIENT_URL="https://drugify.karthikgowdams.co"
 
 module.exports = {
   CLIENT_URL
