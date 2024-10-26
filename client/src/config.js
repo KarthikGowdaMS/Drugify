9d0f478ff9cc7402cbaefcf0fbe916d9e68450a5
Karthik Gowda M S <karthikgowdams27@gmail.com>
2024-10-26 22:52:47 +0530

    Update config.js

--- client/src/config.js
+++ client/src/config.js
@@ -1,5 +1,5 @@
 // const BASE_URL='http://localhost:5000/api'
 // const BASE_URL = 'http://192.168.163.141:5000/api';
-const BASE_URL = 'https://drugify-backend.karthikgowdams.com/api';
+const BASE_URL = 'https://drugify-backend.karthikgowdams.co/api';
 
 export default BASE_URL;
